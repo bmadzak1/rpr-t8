11b16b39c85146ed2cdaa7802618af16111c5f77
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-12-17 21:40:36 +0100

    Dodan Search

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.awt.*;
@@ -13,10 +15,20 @@ public class Controller {
 
     public TextField searchField;
     public Button searchBtn;
+    public ListView<String> searchList;
     SimpleStringProperty searchText;
+    Thread search;
 
     public Controller(){
         searchText = new SimpleStringProperty("");
+    }
+
+    public ListView getSearchList() {
+        return searchList;
+    }
+
+    public void setSearchList(ObservableList<String> list) {
+        searchList.itemsProperty().setValue(list);
     }
 
     public SimpleStringProperty searchTextProperty(){
@@ -33,6 +45,9 @@ public class Controller {
     }
 
     public void btnPressed(ActionEvent actionEvent) {
-
+        if(searchField.textProperty().get() == "")
+            return;
+        search = new Thread(new Search(searchField.textProperty().get(), this));
+        search.start();
     }
 }
---
+++ src/sample/Search.java
@@ -0,0 +1,65 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Search implements Runnable {
+
+    String text;
+    Controller controller;
+    ObservableList<String> fileList;
+    File searchFile;
+
+    public ObservableList<String> getSearchResult()
+    {
+        return fileList;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < searchFile.list().length; i++) {
+            File newFile = new File(searchFile, searchFile.list()[i]);
+            searchByFile(newFile);
+        }
+        Platform.runLater(() -> controller.setSearchList(fileList));
+    }
+
+    Search(String searchText, Controller controller)
+    {
+        fileList = FXCollections.observableArrayList();
+        searchFile = new File("C:\\Users\\pc");
+        this.controller = controller;
+        text = searchText;
+    }
+
+    void searchByFile(File f)
+    {
+        int firstSubstringLetter = 0;
+        int lastSubstringLetter = text.length();
+        while (lastSubstringLetter <= f.getName().length()) {
+            if(f.getName().substring(firstSubstringLetter, lastSubstringLetter).equalsIgnoreCase(text)) {
+                fileList.add(f.getPath());
+                break;
+            }
+            firstSubstringLetter++;
+            lastSubstringLetter++;
+        }
+
+        if(f.list() != null)
+            for(int i = 0; i < f.list().length; i++) {
+                File newFile = new File(f, f.list()[i]);
+                searchByFile(newFile);
+            }
+        else
+            return;
+    }
+}
